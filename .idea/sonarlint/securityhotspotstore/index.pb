456fb123858ca9852e8c52ef2732ee52960036da
dejvvit13 <dawidlasek137@gmail.com>
2023-05-08 14:41:24 +0200

    day4

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+h
+8src/main/java/com/github/dejvvit13/day1/Day1Problem.java,4\2\4273393add2a89c11c70a2e763496a5149746dc2
+h
+8src/main/java/com/github/dejvvit13/day2/Day2Problem.java,0\7\078b891521aafa7434adf464baf821f0de8e46db
+b
+2src/main/java/com/github/dejvvit13/day3/Codec.java,c\b\cb388c6a48bdcdef0e8d9d5ea8816e8297073832
+a
+1src/main/java/com/github/dejvvit13/day3/Node.java,f\8\f86a8ede3fe371df839810de934ea29d5ff7b1b4
+h
+8src/main/java/com/github/dejvvit13/day3/Day3Problem.java,0\d\0d14b48b5a681accc37a9633e86459b63da61eb7
---
+++ src/main/java/com/github/dejvvit13/day4/Day4Problem.java
@@ -0,0 +1,43 @@
+package com.github.dejvvit13.day4;
+
+//Given an array of integers, find the first missing positive integer in linear time and constant space.
+//In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+//For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+import com.sun.source.tree.Tree;
+
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Day4Problem {
+
+    public static void main(String[] args) {
+        int[] array1 = {3, 4, -1, 1};
+        int[] array2 = {1, 2, 0};
+        int[] array3 = {-2, -3, -4, 3, 4, 5, 6, 9, 10};
+        System.out.println(solve(array1));
+        System.out.println(solve(array2));
+        System.out.println(solve(array3));
+    }
+
+    public static int solve(int[] numbers) {
+        Set<Integer> treeSet = new TreeSet<>();
+        int next = 0;
+
+        for (int num : numbers) {
+            if (num > 0) treeSet.add(num);
+        }
+
+        for (final Integer integer : treeSet) {
+            int actual = next;
+            next = integer;
+            if (next - actual > 1) {
+                return actual + 1;
+            }
+        }
+        return next + 1;
+    }
+
+}
